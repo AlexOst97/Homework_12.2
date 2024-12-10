84352d6dff1a5bdf0fcb0d0af731a0181a7361af
unknown <sanya@example.com>
2024-12-10 22:03:45 +0700

    add_2

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
 import json
+import logging
 from typing import Any
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(
+    r"C:\Users\sanya\PycharmProjects\python_project_dz2\logs\utils.log",
+    "w",
+    encoding="utf-8",
+)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(funcName)s - %(levelname)s: %(message)s)"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def transaction(json_file: str) -> Any:
@@ -9,8 +23,10 @@ def transaction(json_file: str) -> Any:
     if json_file is not list and len(json_file) > 0:
         with open(json_file, "r", encoding="utf-8") as file:
             transactions = json.load(file)
+            logger.info(transactions)
             return transactions
     else:
+        logger.info(empty_list)
         return empty_list
 
 
